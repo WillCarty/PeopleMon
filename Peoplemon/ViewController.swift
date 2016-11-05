ed0df51d4f9222eba10ef677fe2e79a0891afebb
Will Carty <will.carty@interapthq.com>
2016-11-04 20:22:56 -0400

    Initial Commit

---
+++ Peoplemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peoplemon
+//
+//  Created by Will Carty on 11/4/16.
+//  Copyright © 2016 Will Carty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
